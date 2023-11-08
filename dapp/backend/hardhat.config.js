c36c5153ecc00d39cb8c05ad4159a12db4c14bfe
chainventive@gmail.com <chainventive@gmail.com>
2023-11-08 17:09:51 +0100

    Add etherscan api key support

--- dapp/backend/hardhat.config.js
+++ dapp/backend/hardhat.config.js
@@ -1,12 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config()
 
-// Go to https://infura.io, sign up, create a new API key
-// in its dashboard, and replace "KEY" with it
 const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
-
-// Replace this private key with your Sepolia account private key
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || '0x0000000000000000000000000000000000000000000000000000000000000000';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -19,6 +16,9 @@ module.exports = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
-    }
+    },
+    etherscan: {
+      apiKey: ETHERSCAN_API_KEY,
+    },
   }
 };
