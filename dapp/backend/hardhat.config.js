d1f4fda369f1622da678293cbd3416ac7f9c15ee
chainventive@gmail.com <chainventive@gmail.com>
2023-11-08 16:15:43 +0100

    Add local hardhat network config

--- dapp/backend/hardhat.config.js
+++ dapp/backend/hardhat.config.js
@@ -13,6 +13,8 @@ module.exports = {
   solidity: "0.8.19",
 
   networks: {
+    hardhat: {
+    },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
