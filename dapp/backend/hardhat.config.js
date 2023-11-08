ca7d99bada3e2b0ee6db3a0f1dd8a285bcbfb1f4
chainventive@gmail.com <chainventive@gmail.com>
2023-11-08 12:02:54 +0100

    Add hardhat config for Sepolia Network deployements

--- dapp/backend/hardhat.config.js
+++ dapp/backend/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// Go to https://infura.io, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const INFURA_API_KEY = "KEY";
+
+// Replace this private key with your Sepolia account private key
+const SEPOLIA_PRIVATE_KEY = "YOUR SEPOLIA PRIVATE KEY";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+
   solidity: "0.8.19",
+
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
