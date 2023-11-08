e47a2df7ab5876187dc46970b87ffea70ac47523
chainventive@gmail.com <chainventive@gmail.com>
2023-11-08 16:23:52 +0100

    update hardhat config

--- dapp/backend/hardhat.config.js
+++ dapp/backend/hardhat.config.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
 
 // Replace this private key with your Sepolia account private key
-const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY || '';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
