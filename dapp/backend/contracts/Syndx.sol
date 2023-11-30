379733d0e31e47501703c38fca9f30379eec1bd8
chainventive <chainventive@gmail.com>
2023-11-30 21:24:20 +0100

    Add Syndx core functions

--- dapp/backend/contracts/Syndx.sol
+++ dapp/backend/contracts/Syndx.sol
@@ -12,8 +12,43 @@ import "./common/SDX.sol";
 
 contract Syndx is Ownable {
 
+    // Keep track of deployed contracts allowed to ask for random number
+    mapping (address => SDX.RandomnessConsumer) randomnessConsumers;
+
+    // Ensure the caller is registered as randomnessConsumer
+    modifier onlyAuthorizedRandomnessConsumers () {
+
+        if (randomnessConsumers[msg.sender].authorized == false) revert ("Unauthorized randomness consumer");
+        _;
+    }
+
     // Syndx contract is owned by its deployer
     constructor() Ownable (msg.sender) {
+
+    }
+
+    // Create a new coproperty contract
+    function createCoproperty() external returns (address) {
+        
+    }
+
+    // Create a new vote token contract
+    function createVoteToken() external returns (address) {
+        
+    }
+
+    // Create a new vote assembly contract
+    function createVoteAssembly() external returns (address) {
+        
+    }
+
+    // Ask for a random number (only authorized contracts are able to call)
+    function requestRandomNumber(SDX.RandomStrategy _strategy) public onlyAuthorizedRandomnessConsumers {
+
+    }
+
+    // Get the random number requested by a consumer. Everybody can access this piece of information.
+    function getRequestedRandomNumber(address _consumer) public returns (uint256) {
 
     }
 }
