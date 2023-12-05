cfdfe67624909468bec6fb4acd87ca48e1235f53
chainventive <chainventive@gmail.com>
2023-12-06 00:28:14 +0100

    Add listen to all events to syndx context

--- dapp/backend/scripts/outputs/deployOutput.js
+++ dapp/backend/scripts/outputs/deployOutput.js
@@ -1,18 +1,18 @@
 module.exports = {
-  "network": "hardhat",
-  "blocknumber": 376,
+  "network": "sepolia",
+  "blocknumber": 4830027,
   "contracts": {
     "chainlink": {
       "vrf": {
-        "subscriptionId": 1,
-        "coordinatorAddress": "0x7A28cf37763279F774916b85b5ef8b64AB421f79"
+        "subscriptionId": 7287,
+        "coordinatorAddress": "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625"
       }
     },
     "syndx": {
-      "address": "0xd0EC100F1252a53322051a95CF05c32f0C174354"
+      "address": "0xc76f0847d3dBA70FBe43Bd5651604776649DB6FA"
     },
     "tokenFactory": {
-      "address": "0xCa57C1d3c2c35E667745448Fef8407dd25487ff8"
+      "address": "0xE566C42D47A0f744f7f90ca59e5083d55B626BE5"
     }
   }
 };
