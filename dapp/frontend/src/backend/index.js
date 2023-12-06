16a23e495b7dab40372d0cfd3724efcd1b3cac65
chainventive <chainventive@gmail.com>
2023-12-06 01:31:47 +0100

    Replace abi source from backend artifact folder to frontend deployOutput file

--- dapp/frontend/src/backend/index.js
+++ dapp/frontend/src/backend/index.js
@@ -1,12 +1,6 @@
 require('dotenv').config();
 
-import syndxArtifact from "../../../backend/artifacts/contracts/Syndx.sol/Syndx.json";
-import tokenFactoryArtifact from "../../../backend/artifacts/contracts/tokens/TokenFactory.sol/TokenFactory.json";
-import copropertyArtifact from "../../../backend/artifacts/contracts/coproperty/Coproperty.sol/Coproperty.json";
-import generalAssemblyArtifact from "../../../backend/artifacts/contracts/assembly/GeneralAssembly.sol/GeneralAssembly.json";
-
 const deployOutput = require('@/backend/outputs/deployOutput');
-
 
 export const backend = {
 
@@ -17,12 +11,12 @@ export const backend = {
     contracts: {
 
         syndx: {
-            abi: syndxArtifact.abi,
+            abi: deployOutput.contracts.syndx.abi,
             address: deployOutput.contracts.syndx.address,
         },
 
         tokenFactory: {
-            abi: tokenFactoryArtifact.abi,
+            abi: deployOutput.contracts.syndx.abi,
             address: deployOutput.contracts.tokenFactory.address,
         },
     }
